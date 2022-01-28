1ca49465b28eee05b9dc25403fd5ad6fc8160ff2
Mariana <73724268+golangis@users.noreply.github.com>
2022-01-28 16:32:21 +0000

    Read lines and stop files - add the paths of the lines to a vector of paths

--- src/Stop.cpp
+++ src/Stop.cpp
@@ -4,6 +4,9 @@
 
 #include "../headers/Stop.h"
 
+void Stop::setID(const int &id) {
+    this->id = id;
+}
 void Stop::setCode(const std::string &code) {
     this->code = code;
 }
@@ -23,9 +26,14 @@ void Stop::setCoordinates(const double &longitude, const double &latitude) {
     this->latitude = latitude;
     this->longitude = longitude;
 }
-
+int Stop::getID() const { return id;}
 std::string Stop::getCode() const { return code; }
 std::string Stop::getName() const { return name; }
 std::string Stop::getZone() const { return zone; }
 double Stop::getLatitude() const { return latitude; }
 double Stop::getLongitude() const { return longitude; }
+
+Stop::Stop(const std::string &code, const std::string &name, const std::string &zone, double latitude, double longitude)
+        : id(id), code(code), name(name), zone(zone), latitude(latitude), longitude(longitude) {
+
+}
